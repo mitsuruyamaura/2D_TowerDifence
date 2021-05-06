1b60f911cfb9cdfb1fdafc52320453b83b79b157
mitsuru_yamaura <komayou326@gmail.com>
2021-05-06 14:52:02 +0900

    敵の移動アニメを４方向分作成。BlendTree を利用して、敵の向き(進行方向)と移動のアニメを同期させる処理を実装。

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -10,20 +10,24 @@ public class EnemyController : MonoBehaviour
     [SerializeField]
     private PathData pathData;
 
+    [Header("移動速度")]
     public float moveSpeed;
 
     private Vector3 currentPos;
 
+    private Animator anim;
+
     
     void Start()
     {
+        TryGetComponent(out anim);
+
+        // 移動する地点を取得
         Vector3[] paths = pathData.pathTranArray.Select(x => x.position).ToArray();
 
+        // 各地点に向けて移動
         transform.DOPath(paths, 1000 / moveSpeed).SetEase(Ease.Linear);
     }
-
-    
-
 
 
     void Update()
@@ -35,18 +39,27 @@ public class EnemyController : MonoBehaviour
     /// 敵の進行方向を取得
     /// </summary>
     private void ChangeAnimeDirection() {
-        // TODO Dubug.Log 部分にアニメ遷移を入れる
 
         if (transform.position.x < currentPos.x) {
+            anim.SetFloat("Y", 0f);
+            anim.SetFloat("X", -1.0f);
+
             Debug.Log("左方向");
         } else if (transform.position.y > currentPos.y) {
+            anim.SetFloat("X", 0f);
+            anim.SetFloat("Y", 1.0f);
+
             Debug.Log("上左向");
         } else if (transform.position.y < currentPos.y) {
+            anim.SetFloat("X", 0f);
+            anim.SetFloat("Y", -1.0f);
+
             Debug.Log("下方向");
         } else {
-            //if (transform.position.x > currentPos.x) {
-                Debug.Log("右方向");
-            //} else
+            anim.SetFloat("Y", 0f);
+            anim.SetFloat("X", 1.0f);
+
+            Debug.Log("右方向");         
         }
 
         currentPos = transform.position;
